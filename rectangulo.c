903a047d123d13b599fb76e9ef529387c07867d4
Hito 0/1 <hito94101@gmail.com>
2025-03-31 14:46:19 +0100

    Add files via upload

---
+++ rectangulo.c
@@ -0,0 +1,25 @@
+void rectangle(void){
+    puts("\n");
+	system("color 2");
+	puts("**************** A **************** B ***************");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	system("color d");
+	puts("**************** C **************** D ***************");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	puts("\t\t *\t\t    *");
+	system("color e");
+	puts("**************** E **************** F ***************");
+}
